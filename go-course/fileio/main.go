cb3f0adea2c14abba6d07dbf653dbec1d42935b1
Philip Giacalone <philip_giacalone@yahoo.com>
2021-12-06 16:44:24 -0800

    Added writeToFile function with append feature

--- go-course/fileio/main.go
+++ go-course/fileio/main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func printUsage() {
-	fmt.Println("Usage: go run main.go <read or write> <file_path> [contents]")
+	fmt.Println("Usage: go run main.go <read> <file_path>")
+	fmt.Println("Usage: go run main.go <write> <file_path> [contents] [append bool]")
+}
+
+func writeToFile(filePath string, contents string, append bool, perm os.FileMode) error {
+	if !append {
+		return ioutil.WriteFile(filePath, []byte(contents+"\n"), perm)
+	}
+
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, perm)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err := f.WriteString(contents + "\n"); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
@@ -18,18 +40,30 @@ func main() {
 
 	switch args[1] {
 	case "read":
+		if len(args) < 3 {
+			printUsage()
+			os.Exit(1)
+		}
 		filePath := args[2]
 		b, err := os.ReadFile(filePath)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error during read attempt:", err)
 			os.Exit(1)
 		}
 		fmt.Println(string(b))
 	case "write":
-		filePath := args[2]
-		err := os.WriteFile(filePath, []byte(args[3]), 0644)
+		if len(args) < 4 {
+			printUsage()
+			os.Exit(1)
+		}
+		append := true
+		if len(args) == 5 && args[4] == "false" {
+			append = false
+		}
+		file := args[2]
+		err := writeToFile(file, args[3], append, 0644)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error during write attempt:", err)
 			os.Exit(1)
 		}
 	default:
